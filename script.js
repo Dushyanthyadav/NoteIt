c4c55f0674e9e459c92f70cf131f67573e95b946
Dushyanth <dushyanthyadav6@gmail.com>
2025-07-30 22:41:30 +0530

    Detect theme and toggle between them is done

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const togglebutton = document.getElementById("theme-toggle");
 
-togglebutton.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    if (document.body.classList.contains("dark")) {
+function setTheme(theme) {
+    if (theme === "dark") {
+        document.body.classList.add("dark");
         togglebutton.textContent = "☀️";
     } else {
+        document.body.classList.remove("dark");
         togglebutton.textContent = "🌙";
     }
+}
+
+function detectAndApplyTheme() {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark" || savedTheme === "light") {
+        setTheme(savedTheme)
+    } else {
+
+        const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        setTheme(prefersDark ? "dark": "light");
+    }
+}
+
+togglebutton.addEventListener("click", () => {
+    const isDark = document.body.classList.contains("dark");
+    const newTheme = isDark ? "light" : "dark";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
 });
+
+detectAndApplyTheme();
